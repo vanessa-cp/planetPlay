d5874ba87da870ac394b4730edf68cf9a551b703
vanessa.cp <vanehom@hotmail.com>
2023-03-21 21:31:44 +0100

    ADD y input

--- src/components/PlayList/index.jsx
+++ src/components/PlayList/index.jsx
@@ -1,11 +1,13 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const dataBase = [
     {
         id: "1",
+
         title: "titulo",
         isFavorite: false,
     },
+
 
     {
         id: "2",
@@ -18,24 +20,36 @@ const dataBase = [
         title: "titulo3",
         isFavorite: false,
     },
-
-
 ];
 
- 
 export const PlayList = () => {
-    const[data, setData] = useState(dataBase);
+    const [data, setData] = useState(dataBase);
+    const [input, setInput]=useState("");
 
-    const deleteById = (idToDelete)=> {
+    const deleteById = (idToDelete) => {
         console.log(idToDelete);
         const newData = data.filter((item) => item.id != idToDelete);
         setData(newData);
-    }
+    };
+
+    const handlerChange = (value) =>{
+        console.log(value);
+        setInput(value);
+        console.log(input);
+
+    };
+
     return (
         <div>
+            <section>
+                <input type="text"
+                value={input}  
+                onChange={(e) => handlerChange(e.target.value) }/>
+                <button>ADD</button>
+            </section>
             {data.map((item) => (
                 <article key={item.id}>
-                    {item.title} -<button onClick={()=>deleteById(item.id)}>x</button>
+                    {item.title} -<button onClick={() => deleteById(item.id)}>x</button>
                 </article>
             ))}
         </div>
